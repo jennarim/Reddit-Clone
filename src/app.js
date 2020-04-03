941e5988771222a7a9739226e906837ca1d4a50f
Jenna Rim <rim.jenna@yahoo.com>
2020-04-03 18:01:50 -0400

    Move route handlers + set up middleware

--- src/app.js
+++ src/app.js
@@ -1,23 +1,53 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const sessionOptions = { 
-    secret: 'secret for signing session id', 
-    saveUninitialized: false, 
-    resave: false 
-};
-app.use(session(sessionOptions));
+require('./db.js');
+require('./auth.js');
 
+const express = require('express'),
+	  path = require('path'),
+	  mongoose = require('mongoose'),
+	  session = require('express-session'),
+	  Handlebars = require('hbs'),
+	  fs = require('fs'),
+	  passport = require('passport');
+
+const router = require('./routes/router.js');
+
+const app = express();
+
+const secretKey = fs.readFileSync(path.join(__dirname, 'session-key.txt'), 'utf8');
+
+const sessionOptions = { 
+    secret: secretKey, 
+    saveUninitialized: true, 
+    resave: true 
+};
 
 app.set('view engine', 'hbs');
+app.use(session(sessionOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-	res.send("test");
+const Category = mongoose.model('Category');
+const Post = mongoose.model('Post');
+
+app.use((req, res, next) => {
+	res.locals.user = req.user;
+	next();
 });
 
+app.use((req, res, next) => {
+	Category.find({}, (err, categories) => {
+		res.locals.categories = categories;
+		next();	
+	});
+});
+
+// const postVar = Handlebars.compile(''+);
+Handlebars.registerPartial('postPartial', fs.readFileSync(path.join(__dirname, 'views/partials/postPartial.hbs'), 'utf8'));
+// Handlebars.registerPartial('partialTemplate', '{{language}} is {{adjective}}');
+console.log(Handlebars.partials);
+
+app.use('/', router);
 
 app.listen(3000);
