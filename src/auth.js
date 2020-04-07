b5250dbd598b50abb4e5421ae0d574b5d7932d10
Jenna Rim <rim.jenna@yahoo.com>
2020-04-07 19:26:28 -0400

    Credit to Prof's slides

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,8 @@
+/*
+	Credit to Professor Versoza's slides on Authentication
+	https://cs.nyu.edu/courses/spring20/CSCI-UA.0480-008/_site/slides/16/auth.html?print-pdf#/1
+*/
+
 // let passport know what strategy you want to use as well as how to serialize and deserialize a user
 const mongoose = require('mongoose'),
 	  passport = require('passport'),
