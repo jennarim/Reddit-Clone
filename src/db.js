b063fcc0ca49db258dad4be82ba5c616ecbbb9c4
Jenna Rim <rim.jenna@yahoo.com>
2020-05-11 15:47:32 -0400

    .

--- src/db.js
+++ src/db.js
@@ -50,6 +50,7 @@ let dbconf;
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
 	dbconf = process.env.dbconf;
+	
 } else {
 	// dbconf = "mongodb://localhost/redditClone";
 	// dbconf = `mongodb+srv://${conf.username}:${conf.password}@redditclone-mnc1c.mongodb.net/redditClone?retryWrites=true&w=majority`;
